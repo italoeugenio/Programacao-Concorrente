b58c3ad71a4e6c4e3d92bd7a6b3063779e3d1431
Ítalo Santana <italoeugenio539@gmail.com>
2024-04-25 22:33:17 -0300

    Refazendo classe Main

--- AtividadeN1_ItaloSantana/src/Main.java
+++ AtividadeN1_ItaloSantana/src/Main.java
@@ -1,46 +1,55 @@
 public class Main {
-
     public static void main(String[] args) {
+
+        // O método main apenas instancia objetos e inicia as threads
         Banco banco = new Banco();
 
-        Loja loja1 = new Loja("Loja 1", banco);
-        Funcionario funcionario1Loja1 = new Funcionario(new Conta(0.0, "Salário Funcionário 1 Loja 1"), new Conta(0.0, "Investimento Funcionário 1 Loja 1"), loja1);
-        Funcionario funcionario2Loja1 = new Funcionario(new Conta(0.0, "Salário Funcionário 2 Loja 1"), new Conta(0.0, "Investimento Funcionário 2 Loja 1"), loja1);
+        Loja loja1 = new Loja(banco, "Loja1");
+        Loja loja2 = new Loja(banco, "Loja2");
 
-        Loja loja2 = new Loja("Loja 2", banco);
-        Funcionario funcionario1Loja2 = new Funcionario(new Conta(0.0, "Salário Funcionário 1 Loja 2"), new Conta(0.0, "Investimento Funcionário 1 Loja 2"), loja2);
-        Funcionario funcionario2Loja2 = new Funcionario(new Conta(0.0, "Salário Funcionário 2 Loja 2"), new Conta(0.0, "Investimento Funcionário 2 Loja 2"), loja2);
+        Cliente cliente1 = new Cliente(banco, loja1, loja2, "Cliente 1");
+        Cliente cliente2 = new Cliente(banco, loja1, loja2, "Cliente 2");
+        Cliente cliente3 = new Cliente(banco, loja1, loja2, "Cliente 3");
+        Cliente cliente4 = new Cliente(banco, loja1, loja2, "Cliente 4");
+        Cliente cliente5 = new Cliente(banco, loja1, loja2, "Cliente 5");
 
-        Cliente[] clientes = new Cliente[5];
-        for (int i = 0; i < 5; i++) {
-            clientes[i] = new Cliente(banco, loja1, loja2);
-            clientes[i].start();
-        }
-
-        funcionario1Loja1.start();
-        funcionario2Loja1.start();
-        funcionario1Loja2.start();
-        funcionario2Loja2.start();
-
+        cliente1.start();
+        cliente2.start();
+        cliente3.start();
+        cliente4.start();
+        cliente5.start();
         try {
-            for (Cliente cliente : clientes) {
-                cliente.join();
-            }
-            funcionario1Loja1.join();
-            funcionario2Loja1.join();
-            funcionario1Loja2.join();
-            funcionario2Loja2.join();
+            cliente1.join();
+            cliente2.join();
+            cliente3.join();
+            cliente4.join();
+            cliente5.join();
         } catch (InterruptedException e) {
-            e.printStackTrace();
+            System.out.println(e);
         }
 
-        System.out.println("Saldos finais:");
-        System.out.println("Conta Loja 1: R$" + loja1.getContaLoja().getSaldo());
-        System.out.println("Conta Loja 2: R$" + loja2.getContaLoja().getSaldo());
-        System.out.println("Conta Funcionário 1 Loja 1: R$" + Funcionario.getSalario());
-        System.out.println("Conta Funcionário 2 Loja 1: R$" + Funcionario.getSalario());
-        System.out.println("Conta Funcionário 1 Loja 2: R$" + Funcionario.getSalario());
-        System.out.println("Conta Funcionário 2 Loja 2: R$" + Funcionario.getSalario());
+        // Verifica o saldo final das lojas
+        System.out.println("Saldo final das lojas: ");
+        System.out.println("Saldo da " + loja1.nomeLoja + ": " + loja1.conta.getSaldo());
+        System.out.println("Saldo da " + loja2.nomeLoja + ": " + loja2.conta.getSaldo());
 
+        // Verifica o saldo final das contas dos funcionários e de investimento
+        System.out.println("\n------------- Saldo final das contas dos funcionários -------------: ");
+        System.out.println("Saldo da conta de " + loja1.primeiroFuncionario.nome + ": " + loja1.primeiroFuncionario.contaSalario.getSaldo());
+        System.out.println("Saldo da conta de investimento de " + loja1.primeiroFuncionario.nome + ": " + loja1.primeiroFuncionario.contaInvestimento.getSaldo());
+        System.out.println("Saldo da conta de " + loja1.segundoFuncionario.nome + ": " + loja1.segundoFuncionario.contaSalario.getSaldo());
+        System.out.println("Saldo da conta de investimento de " + loja1.segundoFuncionario.nome + ": " + loja1.segundoFuncionario.contaInvestimento.getSaldo());
+        System.out.println("Saldo da conta de " + loja2.primeiroFuncionario.nome + ": " + loja2.primeiroFuncionario.contaSalario.getSaldo());
+        System.out.println("Saldo da conta de investimento de " + loja2.primeiroFuncionario.nome + ": " + loja2.primeiroFuncionario.contaInvestimento.getSaldo());
+        System.out.println("Saldo da conta de " + loja2.segundoFuncionario.nome + ": " + loja2.segundoFuncionario.contaSalario.getSaldo());
+        System.out.println("Saldo da conta de investimento de " + loja2.segundoFuncionario.nome + ": " + loja2.segundoFuncionario.contaInvestimento.getSaldo());
+
+        // Verifica o saldo final de cada cliente
+        System.out.println("\n------------- Saldo final dos clientes -------------: ");
+        System.out.println("Saldo do " + cliente1.nome + ": " + cliente1.conta.getSaldo());
+        System.out.println("Saldo do " + cliente2.nome + ": " + cliente2.conta.getSaldo());
+        System.out.println("Saldo do " + cliente3.nome + ": " + cliente3.conta.getSaldo());
+        System.out.println("Saldo do " + cliente4.nome + ": " + cliente4.conta.getSaldo());
+        System.out.println("Saldo do " + cliente5.nome + ": " + cliente5.conta.getSaldo());
     }
 }
